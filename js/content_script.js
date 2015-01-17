4bbf442e33ef30ac8de06f952ba4cf76f66b4724
rosrosros <ros@vertica.dk>
2015-01-17 11:17:20 +0100

    Rate from viaplay

--- js/content_script.js
+++ js/content_script.js
@@ -1,3 +1,36 @@
+
+$(function () {
+
+    var node = $('<input id="movielens_refresh" type="button" value="Refresh" style="position: absolute;z-index: 10000;right: 10px;top: 10px;"/>  ').click(refresh);
+    $("body").prepend(node);
+
+    var credentials = {
+        password: "vertica",
+        userName: "cth@vertica.dk"
+    }
+    $.ajax({
+        type: "POST",
+        url: "https://movielens.org/api/sessions",
+        data: JSON.stringify(credentials),
+        contentType: "application/json;charset=UTF-8"
+    })
+        .done(function (a, b, c) {
+
+
+            refresh();
+
+
+        });
+
+
+    $(window).scroll(function () {
+        refresh();
+
+    });
+
+});
+
+
 function findTitles() {
     return $(".item .line-clamp").map(function() {
         return {
@@ -44,12 +77,13 @@ function findRating(titleString, $element) {
             //TODO: check at titel er det samme som [0]
 
             var result = response.data.searchResults[0],
-                prediction = parseFloat(result.movieUserData.prediction).toFixed(1),
+                //prediction = parseFloat(result.movieUserData.prediction).toFixed(1),
+                prediction = result.movieUserData.prediction,
                 rating = result.movieUserData.rating ? result.movieUserData.rating : '-';
 
             //msg+= " SearchHit:" + result.movie.title + ": " +  prediction + " Rating:" +rating ;
 
-            insertInDom($element, prediction, result.movieUserData.rating);
+            insertInDom($element, prediction, result.movieUserData.rating, result.movieId);
 
 
             console.log(title, prediction, rating)
@@ -66,7 +100,7 @@ function findRating(titleString, $element) {
     return req;
 }
 
-function insertInDom($element, prediction, rating) {
+function insertInDom($element, prediction, rating, movieId) {
     var anchor = $element.closest('.labels > div:first-child');
     var starsElement = anchor.find(".movielens_stars");
     if (!starsElement.length) {
@@ -78,20 +112,22 @@ function insertInDom($element, prediction, rating) {
 
     var isRated = Math.round(rating * 2);
 
-    var starsHtml = "";
     for (var i = 0; i < 10; i++) {
 
         var cls;
-        if (i < isRated)
-            cls = 'rating';
-        else
+        if (isRated) 
+            cls = i < isRated ? 'rating' : 'empty';
+        else 
             cls = i < halfStarCount ? 'prediction' : 'empty';
+        
 
         var half = i % 2 == 1 ? ' right-half' : '';
-        starsHtml += '<span class="movielens_star ' + cls + half + ' "></span>';
+        //var starsHtml = '<span class="movielens_star ' + cls + half + ' "></span>';
+        var node = $('<span data-movie-id="' + movieId + '" data-predicted-rating="' + prediction + '" data-number="' + (i + 1) + '" class="movielens_star ' + cls + half + ' "></span>').click(starClick);
+        starsElement.append(node);
     }
 
-    starsElement.html(starsHtml);
+    //starsElement.html(starsHtml);
 }
 
 function refresh() {
@@ -105,41 +141,35 @@ function refresh() {
     }
 }
 
-$(function() {
+function clearCache() {
+    titlesProcessed = {};
+}
 
-    //console.log('init')
-    //$( "body" ).prepend( "<input id="movielens_refresh" type=\"button\" value=\"Refresh\" onclick=\"refresh()\" style=\"position: absolute;z-index: 10000;right: 10px;top: 10px;\" />  ");
-    var node = $('<input id="movielens_refresh" type="button" value="Refresh" style="position: absolute;z-index: 10000;right: 10px;top: 10px;"/>  ').click(refresh);
-    $("body").prepend(node);
+function starClick(event) {
+    var starNumber = $(this).data('number');
+    var movieId = $(this).data('movie-id'),
+        predictedRating = $(this).data('predicted-rating'),
+        rating = starNumber / 2;
 
-    var credentials = {
-        password: "vertica",
-        userName: "cth@vertica.dk"
+    console.log('click', movieId, rating);
+
+    var data = {
+        movieId: movieId,
+        predictedRating: predictedRating,
+        rating: rating
     }
+
     $.ajax({
-            type: "POST",
-            url: "https://movielens.org/api/sessions",
-            data: JSON.stringify(credentials),
-            contentType: "application/json;charset=UTF-8"
-        })
-        .done(function(a, b, c) {
+        type: "POST",
+        url: "https://movielens.org/api/users/me/ratings",
+        data: JSON.stringify(data),
+        contentType: "application/json;charset=UTF-8"
+    })
+    .done(function (a, b, c) {
 
-
-            refresh();
-
-
-        });
-
-
-    $(window).scroll(function() {
+        clearCache();
         refresh();
 
+
     });
-    // document.getElementById('clickMe').addEventListener('click', function() {
-    // // do stuff
-    // });
-
-
-
-
-});
+}
